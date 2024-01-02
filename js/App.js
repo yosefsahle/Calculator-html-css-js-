ccd57e1b0a8e46bb371b283357858e30c3e6da0b
yosefsahle <yosefsahle48@gmail.com>
2024-01-02 15:33:45 +0300

    JavaScript Start

---
+++ js/App.js
@@ -0,0 +1,25 @@
+//numbers
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
+
+//operations
+const dot = document.getElementById("dot");
+const Clear = document.getElementById("clear");
+const Add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const divide = document.getElementById("divide");
+const multiply = document.getElementById("multiply");
+const equal = document.getElementById("equal");
+const percent = document.getElementById("percent");
+const delet = document.getElementById("delet");
+
+//display
+const screen = document.getElementById("screen");
